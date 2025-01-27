7684d1f273417762f42f67afd4dbc862faa4e063
Abdullah Al Shahed <abdullahalshahedkhan@gmail.com>
2025-01-27 17:29:36 +0600

    update

--- config/cors.php
+++ config/cors.php
@@ -20,10 +20,15 @@ return [
     'allowed_methods' => ['*'],
 
     // Allowed origins for CORS requests
-    // 'allowed_origins' => [
-    //     'https://orfa-ai-frontend.vercel.app', // Next.js frontend
-    //     'https://darkgreen-wasp-153824.hostingersite.com', // Laravel backend (optional, for self-origin requests)
-    // ],
+    'allowed_origins' => [
+        // 'https://orfa-ai-frontend.vercel.app', // Next.js frontend
+        // 'https://darkgreen-wasp-153824.hostingersite.com', // Laravel backend (optional, for self-origin requests)
+        'https://ksquaredsourcedcity.com',
+        'https://dashboard.ksquaredsourcedcity.com',
+        'http://localhost:3000',
+        'http://localhost:3001',
+
+    ],
 
     'allowed_origins' => ['*'],
 
